cf9196fce07745c65d9ffec6d150af6d99e80f06
Stephen Gruzin <stephengrzn@gmail.com>
2022-06-08 17:26:09 -0700

    Forgot to remove the testing return statement

--- authServer.js
+++ authServer.js
@@ -107,7 +107,6 @@ app.post("/login", async (req, res) => {
 });
 
 app.post("/signup", async (req, res) => {
-    return res.json({error: false});
     if (!req.body.userData) {
         return res.json({error: true, errorMessage: "An error occured. Please refresh"});
     }
